89f9859473d97d7a733fb31ced7153c8129b7c5d
Oscar M. Cantu <omcantu@gmail.com>
2025-10-20 23:03:15 -0500

    Remove Arch installation for plasma-discover-flatpak
    Remove Arch-specific flatpak installation command.

--- install/desktop/a-flatpak.sh
+++ install/desktop/a-flatpak.sh
@@ -4,8 +4,6 @@ source ~/.local/share/linuxforge/install/lib/env.sh
 run_pkg_mgr install flatpak
 if [ "$OS_NAME" = "Ubuntu" ]; then
   run_pkg_mgrinstall plasma-discover-backend-flatpak
-elif [ "$OS_NAME" = "Arch" ]; then
-  run_pkg_mgr install plasma-discover-flatpak
 fi
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
