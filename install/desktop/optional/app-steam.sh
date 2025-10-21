c72a3ca786a7476086dc13d6bce3de1d82fd05aa
Oscar M. Cantu <omcantu@gmail.com>
2025-10-20 22:28:43 -0500

    Update app-steam.sh

--- install/desktop/optional/app-steam.sh
+++ install/desktop/optional/app-steam.sh
@@ -8,7 +8,7 @@ if [ "$OS_NAME" = "Ubuntu" ]; then
   run_pkg_mgr install ./steam.deb
   rm steam.deb
   cd -
-elseif [ "$OS_NAME" = "Fedora" ]; then
+elif [ "$OS_NAME" = "Fedora" ]; then
   run_pkg_mgr install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
   sudo $PKG_MGR config-manager setopt fedora-cisco-openh264.enabled=1
   run_pkg_mgr install steam
