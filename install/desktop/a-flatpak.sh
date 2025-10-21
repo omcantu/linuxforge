2e798fa5ec9a0067ae59b559472d047ea92314f7
Oscar M. Cantu <omcantu@gmail.com>
2025-10-20 22:55:54 -0500

    Fix syntax error in a-flatpak.sh for Arch OS check

--- install/desktop/a-flatpak.sh
+++ install/desktop/a-flatpak.sh
@@ -4,7 +4,7 @@ source ~/.local/share/linuxforge/install/lib/env.sh
 run_pkg_mgr install flatpak
 if [ "$OS_NAME" = "Ubuntu" ]; then
   run_pkg_mgrinstall plasma-discover-backend-flatpak
-elif [ "$OS_NAME" = "Arch ]; then
+elif [ "$OS_NAME" = "Arch" ]; then
   run_pkg_mgr install plasma-discover-flatpak
 fi
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
