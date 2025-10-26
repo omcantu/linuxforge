9d891526ee48e52c5fe7eb4db59117066c45d8f1
Oscar M. Cantu <omcantu@gmail.com>
2025-10-25 21:00:16 -0500

    Fix typo in vscode sources list filename

--- install/desktop/app-vscode.sh
+++ install/desktop/app-vscode.sh
@@ -10,7 +10,7 @@ if [ "$OS_NAME" = "Fedora" ]; then
 elif [ "$OS_NAME" = "Ubuntu" ]; then
   cd /tmp
   sudo rm -f /etc/apt/keyrings/packages.microsoft.gpg > /dev/null
-  sudo rm -f /etc/apt/sources.list.d/vsdcode.list > /dev/null
+  sudo rm -f /etc/apt/sources.list.d/vscode.list > /dev/null
   wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
   sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
   echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
