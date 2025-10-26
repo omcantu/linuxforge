fb1c36a425104c43a0ac79578b00e621048bfc11
Oscar M. Cantu <omcantu@gmail.com>
2025-10-25 20:28:03 -0500

    Fix package manager install command syntax

--- install/desktop/a-flatpak.sh
+++ install/desktop/a-flatpak.sh
@@ -3,7 +3,7 @@ source ~/.local/share/linuxforge/install/lib/env.sh
 
 run_pkg_mgr install flatpak
 if [ "$OS_NAME" = "Ubuntu" ]; then
-  run_pkg_mgrinstall plasma-discover-backend-flatpak
+  run_pkg_mgr install plasma-discover-backend-flatpak
 fi
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
