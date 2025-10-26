34a87861c1991d74d5caa50f8b04b14e78023bc7
Oscar M. Cantu <omcantu@gmail.com>
2025-10-25 20:57:44 -0500

    Update VSCode installation script for Ubuntu
    Remove old Microsoft GPG key and source list for VSCode installation on Ubuntu.

--- install/desktop/app-vscode.sh
+++ install/desktop/app-vscode.sh
@@ -9,6 +9,8 @@ if [ "$OS_NAME" = "Fedora" ]; then
   run_pkg_mgr install code
 elif [ "$OS_NAME" = "Ubuntu" ]; then
   cd /tmp
+  sudo rm -f /etc/apt/keyrings/packages.microsoft.gpg > /dev/null
+  sudo rm -f /etc/apt/sources.list.d/vsdcode.list > /dev/null
   wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
   sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
   echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
